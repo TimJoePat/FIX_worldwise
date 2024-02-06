db751203e7fa6f0677ceca6b221df3c46c6e87c0
TimJoePat <TimJoePat88@cox.net>
2024-02-06 16:03:12 -0700

    Still in 213 but its running  - holdthis

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,16 @@
+import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
 
 function CityList({ cities, isLoading }) {
-  return <ul className={styles.CityList}>LIST</ul>;
+  if (isLoading) return <Spinner />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
